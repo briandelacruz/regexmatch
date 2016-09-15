d297cc8d24f6d0b7a6e65efe82d5535dcb8a492b
bddelacruz <bddelacruz@192.168.56.1>
2016-09-15 08:22:40 +0800

    Initial Commit

--- Regexmatch/src/regexmatch/Regexmatch.java
+++ Regexmatch/src/regexmatch/Regexmatch.java
@@ -11,8 +11,14 @@ public class Regexmatch {
         return str.matches("True");
     }
     
+    public static boolean containsFalse(String str){
+        return str.matches(".*false.*");
+    
+    }
+    
     public static void main(String[] args) {
-        System.out.println(isTrue("False"));
+        System.out.println(isTrue("True"));
+        System.out.println(containsFalse("asdcxzfalseasdxz"));
     }
     
 }
