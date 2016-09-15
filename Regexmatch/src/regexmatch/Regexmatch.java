bf993229f0c83f7424d007d9694943e1e9a99e75
bddelacruz <bddelacruz@192.168.56.1>
2016-09-15 08:36:01 +0800

    Initial Commit

--- Regexmatch/src/regexmatch/Regexmatch.java
+++ Regexmatch/src/regexmatch/Regexmatch.java
@@ -5,6 +5,10 @@ package regexmatch;
  *
  * @author bddelacruz
  */
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+        
 public class Regexmatch {
 
     public static boolean isTrue(String str){
@@ -16,9 +20,15 @@ public class Regexmatch {
     
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println(isTrue("True"));
         System.out.println(containsFalse("asdcxzfalseasdxz"));
+        Scanner sc = new Scanner (new FileReader("input.txt"));
+        //Reads the file input.txt
+        
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
     }
     
 }
