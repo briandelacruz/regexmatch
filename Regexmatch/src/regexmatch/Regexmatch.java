54223c6bf7690bd7c4d569a96691b1a290458863
bddelacruz <bddelacruz@192.168.56.1>
2016-09-15 08:16:26 +0800

    Initial Commit

--- Regexmatch/src/regexmatch/Regexmatch.java
+++ Regexmatch/src/regexmatch/Regexmatch.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package regexmatch;
 
 /**
@@ -11,11 +7,12 @@ package regexmatch;
  */
 public class Regexmatch {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static boolean isTrue(String str){
+        return str.matches("True");
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(isTrue("False"));
     }
     
 }
